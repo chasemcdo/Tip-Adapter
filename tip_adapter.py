de2d8f8ff5be5ef385ca2ec0df5ffb7c9380f000
Ritvik Singh <ritviks@nvidia.com>
2023-11-27 21:30:28 -0500

    Fix issue with adapter

--- tip_adapter.py
+++ tip_adapter.py
@@ -273,14 +273,10 @@ class TipAdapter:
     def cache(self) -> Dict[str, torch.Tensor]:
         '''Returns the cache values
         '''
-        adapter_weights = None
-        if self._adapter is not None:
-            adapter_weights = self._adapter.data
-
         return {
             "keys": self._cache_keys, 
             "values": self._cache_values,
-            "adapter_weights": adapter_weights,
+            "adapter_weights": self._adapter.weight.data,
             "clip_weights": self._clip_weights,
             "class_names": self._class_names
         }
@@ -293,7 +289,11 @@ class TipAdapter:
             self._cache_keys = cache["keys"].to(self._device)
             self._cache_values = cache["values"].to(self._device)
             if cache["adapter_weights"] is not None:
-                self._adapter = nn.Parameter(cache["adapter_weights"]).to(self._device)
+                self._adapter = nn.Linear(
+                    self._cache_keys.shape[0], 
+                    self._cache_keys.shape[1], bias=False
+                ).to(self._clip_model.dtype).to(self._device)
+                self._adapter.weight = nn.Parameter(cache["adapter_weights"]).to(self._device)
             self._clip_weights = cache["clip_weights"].to(self._device)
             self._class_names = cache["class_names"]
         except KeyError as e:
